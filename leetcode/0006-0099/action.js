f0e3774c3fd74a6812a9224d9d483cdc468ed8d1
lihuiming <sclihuiming@163.com>
2018-10-31 23:57:29 +0800

    leetcode 第11题 参考解答后的优化

--- leetcode/0006-0099/action.js
+++ leetcode/0006-0099/action.js
@@ -178,6 +178,19 @@ let maxArea = function (height) {
     }
     return area;
 };
+//method two
+let maxArea2 = function (height) {
+    let area = 0, l = 0, r = height.length - 1;
+    while (l < r) {
+        area = Math.max(area, Math.min(height[l], height[r]) * (r - l));
+        if (height[l] < height[r]) {
+            l++;
+        } else {
+            r--;
+        }
+    }
+    return area;
+};
 
 
 module.exports = {
