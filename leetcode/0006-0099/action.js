d5d14e7414b33a40b9aa49d7db4a0e12d9de0296
lihuiming <sclihuiming@163.com>
2018-11-01 19:15:16 +0800

    leetcode 第12题

--- leetcode/0006-0099/action.js
+++ leetcode/0006-0099/action.js
@@ -192,6 +192,29 @@ let maxArea2 = function (height) {
     return area;
 };
 
+/**
+ * 12题,整数转罗马数字
+ * @param {number} num
+ * @return {string}
+ */
+let intToRoman = function (num) {
+    let values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    let romanWords = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+    let len = values.length;
+    let res = [];
+    for (let i = 0; i < len && num > 0; i++) {
+        if (num < values[i]) {
+            continue;
+        }
+        while (num >= values[i]) {
+            num -= values[i];
+            res.push(romanWords[i]);
+        }
+    }
+    return res.join('');
+};
+
+
 
 module.exports = {
     convert: convert,
