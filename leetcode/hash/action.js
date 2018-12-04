0fe2b6a839017ed88a8b55142a609926f5a9ccb1
lihuiming <sclihuiming@163.com>
2018-12-05 00:12:04 +0800

    leetcode 第37题 解数独

--- leetcode/hash/action.js
+++ leetcode/hash/action.js
@@ -34,8 +34,87 @@ let isValidSudoku = function (board) {
 };
 
 
-// console.log(isValidSudoku([
-//     ["8", "3", ".", ".", "7", ".", ".", ".", "."],
+/**
+ * 37. 解数独
+ * @param {character[][]} board
+ * @return {void} Do not return anything, modify board in-place instead.
+ */
+let solveSudoku = function (board) {
+    //记录行的数字
+    let row = [];
+    //记录列的数字
+    let col = [];
+    //记录块的数字
+    let block = [];
+    for (let i = 0; i < board.length; i++) {
+        row[i] = [];
+        col[i] = [];
+        block[i] = [];
+        for (let j = 0; j < 10; j++) {
+            row[i][j] = false;
+            col[i][j] = false;
+            block[i][j] = false;
+        }
+    }
+
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board.length; j++) {
+            let blockIndex = Math.floor(i / 3) * 3 + Math.floor(j / 3);
+            if (board[i][j] !== '.') {
+                let num = +board[i][j];
+                row[i][num] = true;
+                col[j][num] = true;
+                // blockIndex = i / 3 * 3 + j / 3，取整
+                block[blockIndex][num] = true;
+            }
+        }
+    }
+    dfs(board, row, col, block, 0, 0);
+    //打印
+    for (let i = 0; i < 9; i++) {
+        let msg = ''
+        for (let j = 0; j < 9; j++) {
+            msg += board[i][j] + " ";
+        }
+        console.log(msg);
+    }
+
+    function dfs(board, row, col, block, i, j) {
+        while (board[i][j] !== '.') {
+            if (++j >= 9) {
+                i++;
+                j = 0;
+            }
+            if (i >= 9) {
+                return true;
+            }
+        }
+        let blockIndex = Math.floor(i / 3) * 3 + Math.floor(j / 3);
+        for (let num = 1; num <= 9; num++) {
+            if (!row[i][num] && !col[j][num] && !block[blockIndex][num]) {
+                // 递归
+                board[i][j] = "" + num;
+                row[i][num] = true;
+                col[j][num] = true;
+                block[blockIndex][num] = true;
+                if (dfs(board, row, col, block, i, j)) {
+                    return true;
+                } else {
+                    // 回溯
+                    row[i][num] = false;
+                    col[j][num] = false;
+                    block[blockIndex][num] = false;
+                    board[i][j] = '.';
+                }
+            }
+        }
+        return false;
+    }
+};
+
+
+// console.log(solveSudoku([
+//     ["5", "3", ".", ".", "7", ".", ".", ".", "."],
 //     ["6", ".", ".", "1", "9", "5", ".", ".", "."],
 //     [".", "9", "8", ".", ".", ".", ".", "6", "."],
 //     ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
@@ -45,3 +124,5 @@ let isValidSudoku = function (board) {
 //     [".", ".", ".", "4", "1", "9", ".", ".", "5"],
 //     [".", ".", ".", ".", "8", ".", ".", "7", "9"]
 // ]));
+
+
