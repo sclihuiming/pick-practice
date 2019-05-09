8ee5e71365df6c5bc08c9bb916ada4f5c941d7e1
lihuiming <sclihuiming@163.com>
2019-05-09 16:17:35 +0800

    二叉树的前序和后序遍历

--- js/tree.js
+++ js/tree.js
@@ -16,14 +16,79 @@ let inorderTraversal = function (root) {
     left = left.left;
   }
   let item = stack.pop();
-  while(item){
+  while (item) {
     ret.push(item.val);
-    let tmp  = item.right;
+    let tmp = item.right;
 
-    while(tmp){
-        stack.push(tmp);
-        tmp = tmp.left;
+    while (tmp) {
+      stack.push(tmp);
+      tmp = tmp.left;
     }
+    item = stack.pop();
   }
   return ret;
 }
+
+/**
+ * 二叉树基于迭代的前序遍历
+ * @param {*} root 
+ */
+let preorderTraversal = function (root) {
+  let ret = [];
+  if (!root) {
+    return ret;
+  }
+  let stack = [root];
+  let item = stack.pop();
+  while (item) {
+    ret.push(item.val);
+    let left = item.left;
+    let right = item.right;
+    if (right) {
+      stack.push(right);
+    }
+    if (left) {
+      stack.push(left);
+    }
+    item = stack.pop();
+  }
+  return ret;
+}
+
+/**
+ * 二叉树基于迭代的后续遍历
+ * @param {*} root 
+ */
+let postorderTraversal = function (root) {
+  let ret = [];
+  if (!root) {
+    return ret;
+  }
+  let stack = [root];
+  let last = root;//用于标记上一次的元素，这里用root进行初始化
+
+  while (stack.length > 0) {
+    let item = stack[stack.length - 1];
+    if (item.left === last ||
+      item.right === last || //子节点已经遍历过了
+      (item.left === null && item.right === null)) { //叶子节点为空
+      last = stack.pop();
+      ret.push(item.val);
+    } else {
+      let left = item.left;
+      let right = item.right;
+      if (right) {
+        stack.push(right);
+      }
+      if (left) {
+        stack.push(left);
+      }
+    }
+  }
+
+  return ret;
+}
+
+
+
+
