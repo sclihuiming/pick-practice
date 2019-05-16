52df525f41d05a7e60555f9afeac564f7d6e2ead
lihuiming <sclihuiming@163.com>
2019-05-16 12:28:55 +0800

    快速排序

--- js/sort.js
+++ js/sort.js
@@ -162,4 +162,44 @@ let mergeSort = function (sourceArray) {
   return sort(sourceArray);
 }
 
+/**
+ * 快速排序
+ * @param {*} sourceArray 
+ */
+let quickSort = function (sourceArray) {
+  if (Object.prototype.toString.call(sourceArray).slice(8, -1) !== 'Array') {
+    return new Error('not a array')
+  }
+  if (sourceArray.length < 2) {
+    return sourceArray;
+  }
+  //排序
+  function sort(array, left, right) {
+    if (left < right) {
+      let index = partition(array, left, right);
+      sort(array, left, index - 1);
+      sort(array, index + 1, right);
+    }
+    return array;
+  }
+  //选定left为基准点，小于left的放在左边，大于left的放在右边
+  function partition(array, left, right) {
+    let index = left + 1;//从left右边第一个开始比较
+    for (let i = index; i <= right; i++) {
+      if (array[i] < array[left]) {
+        if (i !== index) {
+          [array[i], array[index]] = [array[index], array[i]];
+        }
+        index++;
+      }
+    }
+    [array[left], array[index - 1]] = [array[index - 1], array[left]];
+    return index - 1;
+  }
+  return sort(sourceArray,0 ,sourceArray.length);
+}
+
+
+
+
 
